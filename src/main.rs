50f09cb8e2c7859e5a280f8ef1603b7c059a2a05
CGB145 <b.caglan@outlook.com>
2025-09-21 23:59:20 +0300

    test

---
+++ src/main.rs
@@ -0,0 +1,89 @@
+//use sysinfo::{ Components, Disks, Networks, System,};
+use std::fs::File;
+use std::io::prelude::*;
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>());
+}
+
+fn round_to_nth_digit(x:f64, digit: usize) -> f64{
+    let rounded:String = x.to_string();
+
+    let rounded = &rounded[..digit];
+    let rounded = rounded.parse::<f64>().expect("Err");
+    rounded
+}
+
+fn get_numbers(string_with_numbers_placeholder:String, mut string_with_numbers:String) -> String{
+    let numbers:[char;10] = ['0', '1','2','3','4','5','6','7','8','9'];
+    for char in string_with_numbers_placeholder.chars()  {
+
+        if numbers.contains(&char){
+            string_with_numbers.push(char);
+        }
+
+    }
+    string_with_numbers
+}
+
+fn find_memory_entry(file:String, entry_name:&String) -> String{
+
+    // find index of entry searched
+    let location_of_entry: usize = file.find(entry_name).expect("Err");
+
+    // save content beginning at the entry
+    let mut entry = &file[location_of_entry..];
+    // save location of next kB
+    let entry_end_location: usize = entry.find("kB").expect("Err");
+    // save only the entry itself
+    entry = &entry[..entry_end_location];
+
+
+    let placeholder_for_numbers: String = String::from("");
+    let entry: String = get_numbers(entry.to_string(),placeholder_for_numbers);
+    entry
+}
+
+fn main() {
+    // Memory
+    // Open linux Memory Info file
+    let file = File::open("/proc/meminfo");
+    // create String to push content to
+    let mut memory_contents = String::new();
+    // push content to created string
+    match file.expect("REASON").read_to_string(&mut memory_contents){
+        Ok(_) => {},
+        Err(_) =>{println!("Error reading file")},
+    }
+
+    // print content of file
+    println!("{}", memory_contents);
+
+    // find desired entry in string -> convert output to f64 -> kb to gb -> round to nearest 2
+    let mut string = String::from("MemTotal");
+    let mem_total: String=find_memory_entry(memory_contents.to_string(),&string);
+    let mut mem_total:f64 = mem_total.parse::<f64>().expect("Err");
+    mem_total = mem_total/1024.0/1024.0;
+    mem_total = round_to_nth_digit(mem_total, 5);
+
+    // same
+    string.clear();
+    string.push_str("MemFree");
+    let mem_free: String=find_memory_entry(memory_contents.to_string(),&string);
+    let mut mem_free:f64 = mem_free.parse::<f64>().expect("Err");
+    mem_free = mem_free/1024.0/1024.0;
+    mem_free = round_to_nth_digit(mem_free, 4);
+
+    // same
+    string.clear();
+    string.push_str("MemAvailable");
+    let mem_available: String = find_memory_entry(memory_contents.to_string(), &string);
+    let mut mem_available: f64 = mem_available.parse::<f64>().expect("Err");
+    mem_available = mem_available / 1024.0 / 1024.0;
+    mem_available = round_to_nth_digit(mem_available, 4);
+
+    println!("MemTotal: {}", mem_total);
+    println!("MemFree: {}", mem_free);
+    println!("MemAvailable: {}", mem_available);
+
+}
